c764c103c642d7ad8e84fb5f49368b93c4205fda
JosiahFu <josiahfu@gmail.com>
2023-11-10 00:32:26 -0800

    Initial musket implementation

---
+++ src/main/java/archives/tater/unbalancedmusket/MusketItem.java
@@ -0,0 +1,261 @@
+package archives.tater.unbalancedmusket;
+
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
+import net.minecraft.item.*;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.stat.Stats;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.UseAction;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.NotNull;
+import org.joml.Quaternionf;
+import org.joml.Vector3f;
+
+import java.util.function.Predicate;
+
+public class MusketItem extends RangedWeaponItem implements Vanishable {
+
+    public static final String LOADING_STAGE_KEY = "LoadingStage";
+    public static final String PROJECTILE_KEY = "ProjectileItem";
+    public static final float SOUND_PROGRESS_STEP = 0.2F;
+
+    public static class Stage {
+        public static final int UNLOADED = 0;
+        public static final int POWDERED = 1;
+        public static final int RAMMED   = 2;
+        public static final int LOADED   = 3;
+    }
+
+    public MusketItem(Item.Settings settings) {
+        super(settings);
+    }
+
+    private float nextSoundProgressTarget;
+
+
+    @Override
+    public Predicate<ItemStack> getProjectiles() {
+        return itemStack -> itemStack.isOf(Items.GUNPOWDER) || itemStack.isOf(Items.STICK) || itemStack.isOf(Items.ARROW);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, @NotNull PlayerEntity user, Hand hand) {
+        ItemStack musket = user.getStackInHand(hand);
+
+        int stage = getLoadingStage(musket);
+
+        if (stage == Stage.LOADED) {
+            shoot(world, user, hand, musket, getProjectile(musket), 1F, user.isCreative(), 10F, 0.2F);
+            setLoadingStage(musket, 0);
+            return TypedActionResult.consume(musket);
+        }
+
+        ItemStack otherItem = user.getStackInHand(hand == Hand.MAIN_HAND ? Hand.OFF_HAND : Hand.MAIN_HAND);
+
+        if (!isNextItem(stage, otherItem)) return TypedActionResult.fail(musket);
+
+        user.setCurrentHand(hand);
+        nextSoundProgressTarget = 0;
+
+        return TypedActionResult.consume(musket);
+    }
+
+    @Override
+    public void onStoppedUsing(ItemStack stack, World world, LivingEntity user, int remainingUseTicks) {
+        int usedTicks = this.getMaxUseTime(stack) - remainingUseTicks;
+        float progress = getPullProgress(usedTicks, stack);
+        int stage = getLoadingStage(stack);
+
+        if (stage != Stage.RAMMED && progress < 1.0F) return;
+
+        boolean result = loadProjectile(user, stack, user.getOffHandStack(), !(user instanceof PlayerEntity player) || player.isCreative());
+        if (!result) return;
+        world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ITEM_CROSSBOW_LOADING_END, SoundCategory.PLAYERS, 1.0F, 1.0F / (world.getRandom().nextFloat() * 0.5F + 1.0F) + 0.2F);
+    }
+
+    private boolean loadProjectile(LivingEntity shooter, ItemStack musket, ItemStack projectile, boolean creative) {
+        if (projectile.isEmpty()) return false;
+        if (!getProjectiles().test(projectile)) return false;
+        int loadingStage = getLoadingStage(musket);
+        if (!isNextItem(loadingStage, projectile)) return false;
+
+        ItemStack itemStack;
+        if (creative || loadingStage == Stage.POWDERED) {
+            itemStack = projectile;
+        } else {
+            itemStack = projectile.split(1);
+            if (projectile.isEmpty() && shooter instanceof PlayerEntity) {
+                ((PlayerEntity)shooter).getInventory().removeOne(projectile);
+            }
+        }
+
+        switch(loadingStage) {
+            case Stage.UNLOADED -> setLoadingStage(musket, Stage.POWDERED);
+            case Stage.POWDERED -> setLoadingStage(musket, Stage.RAMMED);
+            case Stage.RAMMED -> {
+                setLoadingStage(musket, Stage.LOADED);
+                setProjectile(musket, itemStack);
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean isNextItem(int stage, ItemStack itemStack) {
+        Item item = itemStack.getItem();
+
+        return (
+                stage == Stage.UNLOADED && item == Items.GUNPOWDER ||
+                stage == Stage.POWDERED && item == Items.STICK ||
+                stage == Stage.RAMMED && item == Items.ARROW
+        );
+    }
+
+    public static ItemStack getProjectile(ItemStack itemStack) {
+        NbtCompound nbtCompound = itemStack.getNbt();
+        if (nbtCompound == null) return ItemStack.EMPTY;
+        if (!nbtCompound.contains(PROJECTILE_KEY)) return ItemStack.EMPTY;
+        return ItemStack.fromNbt(nbtCompound.getCompound(PROJECTILE_KEY));
+    }
+
+    private static void setProjectile(ItemStack musket, ItemStack projectile) {
+        NbtCompound musketNbt = musket.getOrCreateNbt();
+        NbtCompound itemNbt = new NbtCompound();
+        projectile.writeNbt(itemNbt);
+        musketNbt.put(PROJECTILE_KEY, itemNbt);
+    }
+
+    private static void clearProjectile(ItemStack musket) {
+        NbtCompound nbtCompound = musket.getNbt();
+        if (nbtCompound == null) return;
+        nbtCompound.remove(PROJECTILE_KEY);
+    }
+
+    public static int getLoadingStage(ItemStack stack) {
+        NbtCompound nbtCompound = stack.getNbt();
+        return nbtCompound == null ? 0 : nbtCompound.getInt(LOADING_STAGE_KEY);
+    }
+
+    public static void setLoadingStage(ItemStack stack, int stage) {
+        NbtCompound nbtCompound = stack.getOrCreateNbt();
+        nbtCompound.putInt(LOADING_STAGE_KEY, stage);
+    }
+
+    private static void shoot(World world, LivingEntity shooter, Hand hand, ItemStack musket, ItemStack projectile, float soundPitch, boolean creative, float speed, float divergence) {
+        if (world.isClient) return;
+
+        PersistentProjectileEntity projectileEntity = createArrow(world, shooter, musket, projectile);
+
+        if (creative) {
+            projectileEntity.pickupType = PersistentProjectileEntity.PickupPermission.CREATIVE_ONLY;
+        }
+
+        Vec3d vec3d = shooter.getOppositeRotationVector(1.0F);
+        Quaternionf quaternionf = (new Quaternionf()).setAngleAxis(0, vec3d.x, vec3d.y, vec3d.z);
+        Vec3d vec3d2 = shooter.getRotationVec(1.0F);
+        Vector3f vector3f = vec3d2.toVector3f().rotate(quaternionf);
+        projectileEntity.setVelocity(vector3f.x(), vector3f.y(), vector3f.z(), speed, divergence);
+
+        musket.damage(1, shooter, (e) -> e.sendToolBreakStatus(hand));
+        world.spawnEntity(projectileEntity);
+        world.playSound(null, shooter.getX(), shooter.getY(), shooter.getZ(), SoundEvents.ITEM_CROSSBOW_SHOOT, SoundCategory.PLAYERS, 1.0F, soundPitch);
+
+        if (shooter instanceof ServerPlayerEntity serverPlayerEntity) {
+//            Criteria.SHOT_CROSSBOW.trigger(serverPlayerEntity, musket);
+
+            serverPlayerEntity.incrementStat(Stats.USED.getOrCreateStat(musket.getItem()));
+        }
+
+        clearProjectile(musket);
+    }
+
+    private static PersistentProjectileEntity createArrow(World world, LivingEntity entity, ItemStack crossbow, ItemStack arrow) {
+        ArrowItem arrowItem = (ArrowItem)(arrow.getItem() instanceof ArrowItem ? arrow.getItem() : Items.ARROW);
+        PersistentProjectileEntity persistentProjectileEntity = arrowItem.createArrow(world, arrow, entity);
+        if (entity instanceof PlayerEntity) {
+            persistentProjectileEntity.setCritical(true);
+        }
+
+        persistentProjectileEntity.setSound(SoundEvents.ITEM_CROSSBOW_HIT);
+        persistentProjectileEntity.setShotFromCrossbow(true);
+        int i = EnchantmentHelper.getLevel(Enchantments.PIERCING, crossbow);
+        if (i > 0) {
+            persistentProjectileEntity.setPierceLevel((byte)i);
+        }
+
+        return persistentProjectileEntity;
+    }
+
+    public void usageTick(World world, LivingEntity user, ItemStack stack, int remainingUseTicks) {
+        if (world.isClient) return;
+        int loadingStage = getLoadingStage(stack);
+        if (loadingStage == Stage.RAMMED || loadingStage == Stage.LOADED) return;
+
+        float progress = (float)(stack.getMaxUseTime() - remainingUseTicks) / (float)getPullTime(stack);
+        if (progress > 1F) return;
+        if (progress <= nextSoundProgressTarget) return;
+        SoundEvent soundEvent = loadingStage == Stage.UNLOADED ? SoundEvents.BLOCK_SAND_PLACE :
+            SoundEvents.BLOCK_SAND_STEP;
+        world.playSound(null, user.getX(), user.getY(), user.getZ(), soundEvent, SoundCategory.PLAYERS, 0.5F, 1.0F);
+        nextSoundProgressTarget += SOUND_PROGRESS_STEP;
+    }
+
+    public int getMaxUseTime(ItemStack stack) {
+        return getPullTime(stack) + 3;
+    }
+
+    public static int getPullTime(ItemStack stack) {
+        int quickChargeLevel = EnchantmentHelper.getLevel(Enchantments.QUICK_CHARGE, stack);
+        return quickChargeLevel == 0 ? 100 : 100 - 20 * quickChargeLevel;
+    }
+
+    public UseAction getUseAction(ItemStack stack) {
+        return UseAction.BOW;
+    }
+
+    private static float getPullProgress(int useTicks, ItemStack stack) {
+        float f = (float)useTicks / (float)getPullTime(stack);
+        if (f > 1.0F) {
+            f = 1.0F;
+        }
+
+        return f;
+    }
+
+//    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+//        List<ItemStack> list = getProjectiles(stack);
+//        if (isCharged(stack) && !list.isEmpty()) {
+//            ItemStack itemStack = list.get(0);
+//            tooltip.add(Text.translatable("item.minecraft.crossbow.projectile").append(ScreenTexts.SPACE).append(itemStack.toHoverableText()));
+//            if (context.isAdvanced() && itemStack.isOf(Items.FIREWORK_ROCKET)) {
+//                List<Text> list2 = Lists.newArrayList();
+//                Items.FIREWORK_ROCKET.appendTooltip(itemStack, world, list2, context);
+//                if (!list2.isEmpty()) {
+//                    list2.replaceAll(text -> Text.literal("  ").append(text).formatted(Formatting.GRAY));
+//
+//                    tooltip.addAll(list2);
+//                }
+//            }
+//
+//        }
+//    }
+
+    public boolean isUsedOnRelease(ItemStack stack) {
+        return stack.isOf(this);
+    }
+
+    public int getRange() {
+        return 8;
+    }
+}
+
---
+++ src/main/java/archives/tater/unbalancedmusket/TotallyBalancedMusket.java
@@ -0,0 +1,32 @@
+package archives.tater.unbalancedmusket;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TotallyBalancedMusket implements ModInitializer {
+	public static final String MOD_ID = "unbalancedmusket";
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	public static final Item MUSKET = new MusketItem(new FabricItemSettings().maxCount(1).maxDamage(200));
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		Registry.register(Registries.ITEM, new Identifier(MOD_ID, "musket"), MUSKET);
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
