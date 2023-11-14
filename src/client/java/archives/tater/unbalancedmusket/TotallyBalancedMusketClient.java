4af29d6fb4d3541a553bd9a6d1285d186a1e7777
JosiahFu <josiahfu@gmail.com>
2023-11-14 13:39:51 -0800

    Add loaded musket pose and first-person poses

--- src/client/java/archives/tater/unbalancedmusket/TotallyBalancedMusketClient.java
+++ src/client/java/archives/tater/unbalancedmusket/TotallyBalancedMusketClient.java
@@ -2,9 +2,11 @@ package archives.tater.unbalancedmusket;
 
 import archives.tater.unbalancedmusket.client.render.entity.MusketBallEntityRenderer;
 import archives.tater.unbalancedmusket.client.render.entity.model.MusketBallEntityModel;
+import archives.tater.unbalancedmusket.item.MusketItem;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.minecraft.client.item.ModelPredicateProviderRegistry;
 import net.minecraft.client.render.entity.model.EntityModelLayer;
 import net.minecraft.util.Identifier;
 
@@ -16,5 +18,7 @@ public class TotallyBalancedMusketClient implements ClientModInitializer {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
 		EntityRendererRegistry.register(TotallyBalancedMusket.MusketBallEntityType, MusketBallEntityRenderer::new);
 		EntityModelLayerRegistry.registerModelLayer(MODEL_MUSKET_BALL_LAYER, MusketBallEntityModel::getTexturedModelData);
+
+		ModelPredicateProviderRegistry.register(TotallyBalancedMusket.MUSKET, new Identifier("charged"), (stack, world, entity, seed) -> MusketItem.getLoadingStage(stack) == MusketItem.Stage.LOADED ? 1.0f : 0.0f);
 	}
 }
---
+++ src/client/java/archives/tater/unbalancedmusket/mixin/client/HeldItemRendererMixin.java
@@ -0,0 +1,30 @@
+package archives.tater.unbalancedmusket.mixin.client;
+
+import archives.tater.unbalancedmusket.TotallyBalancedMusket;
+import archives.tater.unbalancedmusket.item.MusketItem;
+import net.minecraft.client.render.item.HeldItemRenderer;
+import net.minecraft.item.CrossbowItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(HeldItemRenderer.class)
+public class HeldItemRendererMixin {
+    @Redirect(
+            method = "renderFirstPersonItem",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isOf(Lnet/minecraft/item/Item;)Z", ordinal = 1)
+    )
+    private boolean allowMusket(ItemStack stack, Item item) {
+        return stack.isOf(item) || stack.isOf(TotallyBalancedMusket.MUSKET);
+    }
+
+    @Redirect(
+            method = "renderFirstPersonItem",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/item/CrossbowItem;isCharged(Lnet/minecraft/item/ItemStack;)Z")
+    )
+    private boolean allowChargedMusket(ItemStack stack) {
+        return CrossbowItem.isCharged(stack) || MusketItem.getLoadingStage(stack) == MusketItem.Stage.LOADED;
+    }
+}
